d1fe6426e05d1d96975d96684ab0254d9a5e142a
Joshua Davies <mopaxton@gmail.com>
2024-10-17 21:05:06 +0100

    wip

---
+++ ScaryDungeonCrawler/Assets/Code/MapGenerator.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public DungeonTile[] tiles = new DungeonTile[6];
+    public bool generateRandomMap;
+    private List<GameObject> deck = new List<GameObject>();
+
+    void Awake()
+    {
+        if(generateRandomMap)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < tiles[i].count; j++)
+                {
+                    deck.Add(Instantiate(tiles[i].prefab));
+                }
+            }
+            deck.Shuffle();
+            var coords = new Vector3();
+            foreach (var item in deck)
+            {
+                item.transform.position = coords;
+                coords.x += 3;
+            }
+        }
+    }
+}
+
+[System.Serializable]
+public class DungeonTile
+{
+    public GameObject prefab;
+    public int count;
+}
+
+public static class IListExtensions
+{
+    /// <summary>
+    /// Shuffles the element order of the specified list.
+    /// </summary>
+    public static void Shuffle<T>(this IList<T> ts)
+    {
+        var count = ts.Count;
+        var last = count - 1;
+        for (var i = 0; i < last; ++i)
+        {
+            var r = UnityEngine.Random.Range(i, count);
+            var tmp = ts[i];
+            ts[i] = ts[r];
+            ts[r] = tmp;
+        }
+    }
+}
--- ScaryDungeonCrawler/Assets/Code/PlayerMovement.cs
+++ ScaryDungeonCrawler/Assets/Code/PlayerMovement.cs
@@ -1,18 +1,87 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float moveSpeed = 10f;
+    public float rotateSpeed = 500f;
+    public float collisionDetectionDistance = 10f;
+    public int turns = 0;
+
+    Vector3 targetPosition;
+    Vector3 targetRotation;
+
+    bool IsMoving
     {
-        
+        get
+        {
+            return !((Vector3.Distance(transform.position, targetPosition) < 0.02f) &&
+                    (Vector3.Distance(transform.eulerAngles, targetRotation) < 0.02f));
+        }
     }
 
-    // Update is called once per frame
+    public void RotateLeft() { if (!IsMoving) targetRotation -= Vector3.up * 90f; }
+    public void RotateRight() { if (!IsMoving) targetRotation += Vector3.up * 90f; }
+    public void MoveForward()
+    {
+        Vector3 fwd = transform.TransformDirection(Vector3.forward) * collisionDetectionDistance;
+
+        if (!Physics.Raycast(transform.position, fwd, out RaycastHit hitinfo, collisionDetectionDistance))
+        {
+            Debug.Log("Nothing in front of you");
+            if (!IsMoving)
+            {
+                Debug.Log("IsMoving: " + IsMoving);
+                targetPosition += transform.forward;
+                turns++;
+            }
+        }
+    }
+
+    public void MoveBackward()
+    {
+        Vector3 bwd = transform.TransformDirection(Vector3.back) * collisionDetectionDistance;
+
+        if (!Physics.Raycast(transform.position, bwd, collisionDetectionDistance))
+        {
+            if (!IsMoving)
+            {
+                targetPosition -= transform.forward;
+                turns++;
+            }
+        }
+    }
+
+    
+    void Start()
+    {
+        targetPosition = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        Vector3 target = targetPosition;
+
+        if (targetRotation.y > 270f && targetRotation.y < 361f) targetRotation.y = 0f;
+        if (targetRotation.y < 0f) targetRotation.y = 270f;
+
+        transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * moveSpeed);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(targetRotation), Time.deltaTime * rotateSpeed);
+    }
+
     void Update()
     {
-        
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * collisionDetectionDistance;
+        Debug.DrawRay(transform.position, forward, Color.green);
+
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.UpArrow)) MoveForward();
+        if (Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.DownArrow)) MoveBackward();
+        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow)) RotateLeft();
+        if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow)) RotateRight();
     }
 }
